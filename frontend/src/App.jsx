1592d25bed333440ac9015da09fd076278fb99d6
latishsalunkhe8@gmail.com <latishsalunkhe8@gmail.com>
2025-07-25 16:06:10 +0530

    SkillSection Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,10 @@
 import AboutMeMain from "./components/aboutMeSection/AboutMeMain";
+import HelperSection from "./components/HelperSection";
 import HeroGradient from "./components/HeroSection/HeroGradient";
 import HeroMain from "./components/HeroSection/HeroMain";
 import NavbarMain from "./components/Navbar/NavbarMain";
+import SkillsMain from "./components/skillSection/SkillsMain";
+import SubSkills from "./components/skillSection/SubSkills";
 import SubHeroMain from "./components/subHeroSection/SubHeroMain";
 
 
@@ -13,6 +16,9 @@ function App() {
     <HeroGradient />
     <SubHeroMain />
     <AboutMeMain />
+    <SkillsMain />
+    <SubSkills />
+    <HelperSection />
    </div>
   )
 }
