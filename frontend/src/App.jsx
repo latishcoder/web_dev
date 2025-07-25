951f1cbd6a158aaac35750582e95fe2befb31264
latishsalunkhe8@gmail.com <latishsalunkhe8@gmail.com>
2025-07-25 21:31:56 +0530

    Experience Section Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import AboutMeMain from "./components/aboutMeSection/AboutMeMain";
+import ExperienceMain from "./components/experienceSection/ExperienceMain";
 import HelperSection from "./components/HelperSection";
 import HeroGradient from "./components/HeroSection/HeroGradient";
 import HeroMain from "./components/HeroSection/HeroMain";
 import NavbarMain from "./components/Navbar/NavbarMain";
+import ProjectsMain from "./components/projectsSection/ProjectsMain";
 import SkillsMain from "./components/skillSection/SkillsMain";
 import SubSkills from "./components/skillSection/SubSkills";
 import SubHeroMain from "./components/subHeroSection/SubHeroMain";
@@ -11,13 +13,15 @@ import SubHeroMain from "./components/subHeroSection/SubHeroMain";
 function App() {
   return(
    <div className="font-body">
-     <NavbarMain />
+    <NavbarMain />
     <HeroMain />
     <HeroGradient />
     <SubHeroMain />
     <AboutMeMain />
     <SkillsMain />
     <SubSkills />
+    <ExperienceMain />
+    <ProjectsMain />
     <HelperSection />
    </div>
   )
